0720a7da91dd23c8b8590eb172880d5755471719
Steven Hernandez <hernandsteven@gmail.com>
2022-07-27 21:37:36 -0400

    change color of button

--- src/components/ActionMenu.tsx
+++ src/components/ActionMenu.tsx
@@ -3,7 +3,7 @@ import { PlusIcon } from '@heroicons/react/outline'
 
 const ActionMenu = () => {
 	return (
-		<div className="flex flex-row flex-1 border rounded-md gap-4 p-4 bg-gray-50">
+		<div className="flex flex-row flex-1 border-2 rounded-md gap-4 p-4 bg-gray-50">
 			<section className="flex flex-row flex-1 gap-4 text-gray-500">
 				<div className="hover:bg-gray-200 outline outline-gray-300 text-center min-w-[5rem] p-2 rounded-md cursor-pointer">
 					Overview
@@ -18,7 +18,7 @@ const ActionMenu = () => {
 					Tasks
 				</div>
 			</section>
-			<button className="flex flex-row justify-center items-center gap-1 h-fit w-fit rounded-md bg-purple-600 hover:bg-purple-500 self-end text-center p-2 cursor-pointer text-white">
+			<button className="flex flex-row justify-center items-center gap-1 h-fit w-fit rounded-md bg-purple-400 hover:bg-purple-500 self-end text-center p-2 cursor-pointer text-white">
 				<PlusIcon className="h-5 w-5" />
 				<p className="whitespace-nowrap"> Add Category </p>
 			</button>
