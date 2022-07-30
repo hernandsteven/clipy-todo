b770bc86b38cf556dca78efb183fbb9bc8115e8b
hernandsteven <58962667+hernandsteven@users.noreply.github.com>
2022-07-30 17:26:39 -0400

    renamed file

---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,123 @@
+import { randomUUID } from 'crypto'
+import { FC } from 'react'
+import { todo } from '../pages'
+import Section from './Section'
+const data = {
+	Todo: [
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+	],
+	Doing: [
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [
+				{ difficulty: 'Easy' },
+				{ difficulty: 'Medium' },
+				{ difficulty: 'Hard' },
+			],
+		},
+	],
+	Done: [
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [{ difficulty: 'Hard' }],
+		},
+		{
+			title: 'Take out the trash',
+			description: 'I have guest coming over tmwr so tidy up the house',
+			tags: [{ difficulty: 'Medium' }],
+		},
+	],
+}
+
+const KanbanBoard: FC<any> = ({ todos }) => {
+	const sections = Object.entries(data)
+	return (
+		<div
+			id="sections_row"
+			className="flex md:flex-row sm:flex-col gap-[.5rem] p-4 overflow-x-scroll max-w-[93vw]"
+		>
+			{sections.map(([title, tasks], idx) => {
+				return (
+					<Section key={Math.random()} title={title} tasks={tasks} />
+				)
+			})}
+			<Section key={Math.random()} title={'Untitled'} />
+			<Section key={Math.random()} title={'Untitled'} />
+		</div>
+	)
+}
+
+export default KanbanBoard
